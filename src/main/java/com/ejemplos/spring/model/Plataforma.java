5ae92a16d321b415570a39be895837b8442bf36c
Alvaro Medina Vena <alvaromedinavena11@gmail.com>
2023-11-22 11:06:40 +0100

    Carga incial y crear Script sql

--- src/main/java/com/ejemplos/spring/model/Plataforma.java
+++ src/main/java/com/ejemplos/spring/model/Plataforma.java
@@ -4,7 +4,7 @@ public enum Plataforma {
 	PS2("PS2"), X360("X360"), PS3("PS3"), WII("Wii"), DS("DS"), PS("PS"), GBA("GB"), PS4("PS4"), PSP("PSP"), PC("PC"),
 	XB("XB"), GC("GC"), _3DS("3DS"), N64("N64"), PSV("PSV"), SAT("SAT"), SNES("SNES"), XOne("XOne"), WiiU("WiiU"),
 	GEN("GEN"), DC("DC"), NES("NES"), NG("NG"), WS("WS"), SCD("SCD"), TG16("TG16"), _2600("2600"), _3DO("3DO"),
-	GG("GG"), PCFX("PCFX"), GB("GBA"), OTRA("OTRA");
+	GG("GG"), PCFX("PCFX"), GB("GBA") , OTRA("OTRA");
 
 	private String valor;
 
