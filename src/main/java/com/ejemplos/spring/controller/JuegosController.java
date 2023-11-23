7fe90871d4d07f6d7b6dd56fba1c106d58cf991a
Alvaro Medina Vena <alvaromedinavena11@gmail.com>
2023-11-23 23:42:14 +0100

    Manejo de errores

--- src/main/java/com/ejemplos/spring/controller/JuegosController.java
+++ src/main/java/com/ejemplos/spring/controller/JuegosController.java
@@ -2,6 +2,8 @@ package com.ejemplos.spring.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ejemplos.spring.model.CustomResponse;
 import com.ejemplos.spring.model.Juego;
 import com.ejemplos.spring.model.JuegoDTO;
 import com.ejemplos.spring.service.JuegoService;
@@ -23,13 +26,14 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-
 import java.util.List;
 
 @RestController
 @RequestMapping("/juegos")
 @Tag(name = "Juegos", description = "Operaciones relacionadas con los juegos")
 public class JuegosController {
+
+	private static final Logger logger = LoggerFactory.getLogger(JuegosController.class);
 
 	@Autowired
 	private JuegoService juegoService;
@@ -39,6 +43,7 @@ public class JuegosController {
 	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Lista de juegos recuperada con éxito"),
 			@ApiResponse(responseCode = "204", description = "No hay juegos en la base de datos") })
 	public ResponseEntity<List<JuegoDTO>> getJuegos() {
+		logger.info("------ readJuegos (GET) ");
 		List<JuegoDTO> juegos = juegoService.getJuego();
 		if (juegos.isEmpty()) {
 			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
@@ -50,24 +55,31 @@ public class JuegosController {
 	@Operation(summary = "Eliminar un juego por ID", description = "Elimina un juego de la base de datos por su ID")
 	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Juego eliminado con éxito"),
 			@ApiResponse(responseCode = "404", description = "No se encontró ningún juego con el ID proporcionado") })
-	public ResponseEntity<JuegoDTO> deleteJuego(
+	public ResponseEntity<?> deleteJuego(
 			@Parameter(description = "ID del juego a eliminar", required = true) @PathVariable int id) {
 		JuegoDTO juegoEliminado = juegoService.deleteJuego(id);
 
 		if (juegoEliminado != null) {
-			return ResponseEntity.ok(juegoEliminado);
+			CustomResponse<JuegoDTO> customResponse = CustomResponse.createSuccessResponse(juegoEliminado);
+			return ResponseEntity.ok(customResponse);
 		} else {
-			return ResponseEntity.notFound().build();
+			CustomResponse<Void> customResponse = CustomResponse
+					.createNotFoundResponse("No se encontró ningún juego con el ID proporcionado");
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(customResponse);
 		}
+
 	}
 
-	@PostMapping
+	@PostMapping("/add")
 	@Operation(summary = "Agregar un nuevo juego", description = "Agrega un nuevo juego a la base de datos")
 	@ApiResponses(value = { @ApiResponse(responseCode = "204", description = "Juego agregado con éxito") })
-	public ResponseEntity<?> addJuego(
-			@Parameter(description = "Datos del juego a agregar", required = true) @RequestBody Juego juego) {
-		juegoService.addJuego(juego);
-		return ResponseEntity.noContent().build();
+	public ResponseEntity<CustomResponse<String>> addJuego(
+	        @Parameter(description = "Datos del juego a agregar", required = true) @RequestBody Juego juego) {
+	    logger.info("------ addJuego (POST)");
+	    juegoService.addJuego(juego);
+	    String mensaje = "Juego agregado con éxito: " + juego.getNombre();
+	    return ResponseEntity.status(HttpStatus.CREATED)
+	            .body(new CustomResponse<>(201, mensaje, null));
 	}
 
 	// Agregar otros métodos, como el de carga inicial, según sea necesario.
